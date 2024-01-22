ef1ccfe5dc64db92594f34e2613193bba32adcf0
ihossain5 <ismail.hossain5169@gmail.com>
2024-01-22 23:56:55 +0600

    category store, category api resource added

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -9,9 +11,13 @@ class CategoryController extends Controller
 {
     public function index(){
         $categpries = Category::query()->select('id','name')->get();
-        
-        return $this->apiSuccessResponse($categpries);
+
+        return $this->apiSuccessResponse(CategoryResource::collection($categpries));
     }
 
-    public function store(Request $request){}
+    public function store(CategoryRequest $request){
+        $category = Category::create($request->validated());
+
+        return $this->apiSuccessResponse(new CategoryResource($category));
+    }
 }
