c49c43da14b2c259683dd456372456e1e7a7364f
ihossain5 <ismail.hossain5169@gmail.com>
2024-01-22 22:54:26 +0600

    api response trait added

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index(){
+        $categpries = Category::query()->select('id','name')->get();
         
+        return $this->apiSuccessResponse($categpries);
     }
 
     public function store(Request $request){}
